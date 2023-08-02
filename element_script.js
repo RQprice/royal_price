d5b0d526bcdf8cb91a12c016ab285676deb5375f
RQprice <roran01@mail.ru>
2023-08-02 23:05:30 +0300

    modified:   element_script.js

--- element_script.js
+++ element_script.js
@@ -52,7 +52,7 @@ function remakeTime(timeString) {
 
 const allPrices = [];
 // Получаем список файлов в папке "data" с расширением .csv
-fetch('/royal_price/data/')
+fetch('https://drive.google.com/drive/folders/1oUk1sUigqWhMauyJvwLfEVCXXqascL6d?usp=sharing')
     .then((response) => response.text())
     .then((data) => {
         const parser = new DOMParser();
